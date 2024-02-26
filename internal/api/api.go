96aa4c9d5c5fe5328e3ab11971ab59edb2ae3f5f
Patrick Dewey <patrickdewey@vt.edu>
2024-02-26 10:37:33 -0500

    feat: api output formatting and tests

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-    "encoding/json"
     "fmt"
     "io"
     "log"
@@ -9,23 +8,15 @@ import (
 )
 
 
-type Team struct {
-    Key string `json:"key"`
-    Number int `json:"team_number"`
-    Name string `json:"name"`
-}
-
-
 // EventList fetches a list of events for a given year from The Blue Alliance API.
 // It takes in a string for the year and a Blue Alliance API key.
 // The output is a tuple containing a string map interface and an error.
 // This function does not modify any global state and has no side effects.
-func EventList(year string, apiKey string) ([]map[string]interface{}, error) {
+func EventList(year string, apiKey string) ([]byte, error) {
     url := fmt.Sprintf("https://www.thebluealliance.com/api/v3/events/%s/simple", year)
 
     // make request to tba api and populate events object
-    var events []map[string]interface{}
-    err := tbaRequest(url, apiKey, events)
+    events, err := tbaRequest(url, apiKey)
     if err != nil {
         log.Fatalf("Error fetching events list: %v", err)
         return nil, err
@@ -35,16 +26,15 @@ func EventList(year string, apiKey string) ([]map[string]interface{}, error) {
 }
 
 
-// EventMatches fetches a json object containing information on all matches for an event.
+// EventMatchesList fetches a json object containing information on all matches for an event.
 // It takes in a string for the event key (i.e. "2023vagle") and a Blue Alliance API key.
 // The output is a tuple containing a string map interface and an error.
 // This function does not modify any global state and has no side effects.
-func EventMatches(eventKey string, apiKey string) ([]map[string]interface{}, error){
+func EventMatchesList(eventKey string, apiKey string) ([]byte, error){
     url := fmt.Sprintf("https://www.thebluealliance.com/api/v3/event/%s/matches", eventKey)
 
     // make request to tba api and populate matches object
-    var matches []map[string]interface{}
-    err := tbaRequest(url, apiKey, matches)
+    matches, err := tbaRequest(url, apiKey)
     if err != nil {
         log.Fatalf("Error fetching matches for event %s: %v", eventKey, err)
         return nil, err
@@ -56,33 +46,31 @@ func EventMatches(eventKey string, apiKey string) ([]map[string]interface{}, err
 
 // EventList fetches a list of teams attending a given event from The Blue Alliance API.
 // It takes in a string for the event key (i.e. "2023vagle") and a Blue Alliance API key.
-// The output is a tuple containing a slice of type Team and an error.
+// The output is a json string
 // This function does not modify any global state and has no side effects.
-func TeamList(eventKey string, apiKey string) ([]Team, error) {
+func TeamList(eventKey string, apiKey string) ([]byte, error) {
     url := fmt.Sprintf("https://www.thebluealliance.com/api/v3/event/%s/teams", eventKey)
 
     // make request to tba api and populate teams object
-    var teams []Team
-    err := tbaRequest(url, apiKey, teams)
+    teams, err := tbaRequest(url, apiKey)
     if err != nil {
         log.Fatalf("Error fetching teams list for event %s: %v", eventKey, err)
         return nil, err
     }
-    
+
     return teams, nil
 }
 
 
 // tbaRequest is a utility function for making requests to the Blue Alliance API.
-// It takes in a 2 strings and a pointer to an interface, which is modified in place.
+// It takes in a 2 strings and outputs a json string
 // This function does not modify the global state. 
-// This function has the side effect of modifying the object passed in as 'target'.
-func tbaRequest(url string, apiKey string, target interface{}) error {
+func tbaRequest(url string, apiKey string) ([]byte, error) {
     // create new request
     req, err := http.NewRequest("GET", url, nil)
     if err != nil {
         log.Fatalf("Error creating request: %v", err)
-        return err
+        return nil, err
     }
 
     // set required headers
@@ -93,7 +81,7 @@ func tbaRequest(url string, apiKey string, target interface{}) error {
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
-        return err
+        return nil, err
     }
     defer resp.Body.Close()
 
@@ -101,16 +89,9 @@ func tbaRequest(url string, apiKey string, target interface{}) error {
     body, err := io.ReadAll(resp.Body)
     if err != nil {
         log.Fatalf("Error reading response body: %v", err)
-        return err
+        return nil, err
     }
 
-    // unmarshal output into go data structure
-    err = json.Unmarshal(body, target)
-    if err != nil {
-        log.Fatalf("Error unmarshalling body: %v", err)
-        return err
-    }
-
-    return nil
+    return body, nil
 }
 
---
+++ internal/api/formatter_test.go
@@ -0,0 +1,155 @@
+package api
+
+import "testing"
+
+// TestEventList calls api.EventList and api.FormatEventList checking for correct output.
+func TestFormatEventList(t *testing.T) {
+    year := "2024"
+    out, err := EventList(year, apiKey)
+    if err != nil {
+        t.Fatalf(`EventList(year, apiKey) = %q. Error: %v`, out, err)
+    }
+
+    // run formatter function
+    events, err := FormatEventList(out)
+    if err != nil {
+        t.Fatalf(`FormatEventList(rawEvents) = %q. Error: %v`, out, err)
+    }
+    
+    // event index in output array
+    ei := 170
+
+    // test correct event key
+    ek := "2024vagle"
+    if events[ei].Key != ek {
+        t.Fatalf("Event at index %d did not match key %s.", ei, ek)
+    }
+
+    // test for correct event name
+    en := "CHS District Glen Allen VA Event"
+    if events[ei].Name != en {
+        t.Fatalf("Event at index %d had mismatched event names %s %s.", ei, en, events[ei].Name)
+    }
+
+    // test for correct event type
+    et := 1
+    if events[ei].EventType != et {
+        t.Fatalf("Event at index %d had mismatched event type %d %d.", ei, et, events[ei].EventType)
+    }
+
+    // test for correct start date
+    sd := "2024-03-15"
+    if events[ei].StartDate != sd {
+        t.Fatalf("Event at index %d had mismatched start date %s %s.", ei, sd, events[ei].StartDate)
+    }
+
+    // test for correct end date
+    ed := "2024-03-17"
+    if events[ei].EndDate != ed {
+        t.Fatalf("Event at index %d had mismatched end date %s %s.", ei, ed, events[ei].EndDate)
+    }
+}
+
+
+// TestEventMatchesList calls api.EventMatchesList and api.FormatMatchList checking for correct output.
+func TestFormatEventMatchesList(t *testing.T) {
+    eventKey := "2020vagle"
+    out, err := EventMatchesList(eventKey, apiKey)
+    if err != nil {
+        t.Fatalf(`EventMatches(eventKey, apiKey) = %q. Error: %v`, out, err)
+    }
+
+    // test formatting function
+    matches, err := FormatMatchList(out)
+    if err != nil {
+        t.Fatalf(`FormatMatchList(rawMatches) = %q. Error %v`, out, err)
+    }
+    
+    // index of match in output array
+    mi := 5
+
+    // test for correct match key
+    mk := "2020vagle_qf1m3"
+    if matches[mi].Key != mk {
+        t.Fatalf("Match at index %d did not match key %s.", mi, mk)
+    }
+
+    // test for correct match number
+    mn := 3
+    if matches[mi].MatchNumber != mn {
+        t.Fatalf("Match at index %d did not match match number %d.", mi, mn)
+    }
+
+    // test for correct comp level
+    cl := "qf"
+    if matches[mi].CompLevel != cl {
+        t.Fatalf("Match at index %d has mismatched comp levels %s %s.", 
+            mi, cl, matches[mi].CompLevel)
+    }
+
+    // test for correct match alliances
+    ra := [3]string{"frc1610", "frc2106", "frc1123"}
+    if matches[mi].Alliances.Red.TeamKeys != ra {
+        t.Fatalf("Expected red alliance %s at %d did not match red alliance %s.",
+            ra, mi, matches[mi].Alliances.Red.TeamKeys)
+    }
+    ba := [3]string{"frc2998", "frc401", "frc6334"}
+    if matches[mi].Alliances.Blue.TeamKeys != ba {
+        t.Fatalf("Expected blue alliance %s at %d did not match blue alliance %s.",
+            ra, mi, matches[mi].Alliances.Blue.TeamKeys)
+    }
+
+    // test for correct score breakdown
+    if matches[mi].ScoreBreakdown == nil {
+        t.Fatalf("Score breakdown at index %d is nil.", mi)
+    }
+    sdr := ScoreDetails{174, 51, 117, 0, 0}
+    if matches[mi].ScoreBreakdown.Red != sdr {
+        t.Fatalf("Score breakdown at index %d did not match expected breakdown", mi)
+    }
+    sdb := ScoreDetails{132, 36, 96, 0, 0}
+    if matches[mi].ScoreBreakdown.Blue != sdb {
+        t.Fatalf("Score breakdown at index %d did not match expected breakdown", mi)
+    }
+
+    // test for correct winning alliance
+    wa := "red"
+    if matches[mi].WinningAlliance != wa {
+        t.Fatalf("Match at index %d had mismatched winning alliances %s %s", 
+            mi, wa, matches[mi].WinningAlliance)
+    }
+}
+
+
+// TestFormatTeamList calls and api.TeamList api.FormatTeamList checking for correct output
+func TestFormatTeamList(t *testing.T) {
+    eventKey := "2020vagle"
+    out, err := TeamList(eventKey, apiKey)
+    if err != nil {
+        t.Fatalf(`TeamList(eventKey, apiKey) = %q. Error: %v`, out, err)
+    }
+
+    // test formatting function
+    teams, err := FormatTeamList(out)
+    if err != nil {
+        t.Fatalf(`FormatTeamList(rawTeams) = %q. Error: %v`, out, err)
+    }
+
+    // index of team within output array
+    teamIndex := 13
+
+    teamKey := "frc2106"
+    if teams[teamIndex].Key != teamKey {
+        t.Fatalf("Team at index %d did not match key %s.", teamIndex, teamKey)
+    }
+
+    teamName := "The Junkyard Dogs"
+    if teams[teamIndex].Name != teamName {
+        t.Fatalf("Team at index %d did not match number %s.", teamIndex, teamName)
+    }
+
+    teamNum := 2106
+    if teams[teamIndex].Number != teamNum {
+        t.Fatalf("Team at index %d did not match number %d.", teamIndex, teamNum)
+    }
+}
